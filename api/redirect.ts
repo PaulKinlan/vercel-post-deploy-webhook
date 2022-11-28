c071fa409312d2177873c2d2bff6320dba36b074
Paul Kinlan <paul.kinlan@gmail.com>
2022-11-28 10:48:33 +0000

    adding qs reference

--- api/redirect.ts
+++ api/redirect.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import * as admin from 'firebase-admin';
+import * as qs from 'qs';
 
 if (!admin.apps.length) {
   admin.initializeApp({
