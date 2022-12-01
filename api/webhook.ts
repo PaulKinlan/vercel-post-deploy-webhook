7f0ed973448ee59bab5cf61f9ab35b1d064f5aff
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-01 20:30:35 +0000

    Formatting

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,13 +1,13 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import * as admin from 'firebase-admin';
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    })
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
@@ -24,33 +24,36 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   const team_id = body.payload.team.id;
 
   if (body.type != "deployment.succeeded") {
-    console.log("incorrect web hook event", body.type)
-    return res.status(404).end("incorrect event")
+    console.log("incorrect web hook event", body.type);
+    return res.status(404).end("incorrect event");
   }
 
   console.log(body.meta);
 
-  const configurationResult = await db.collection('configuration')
+  const configurationResult = await db
+    .collection("configuration")
     .where("team_id", "==", team_id)
-    .where(id, "!=", "").get();
+    .where(id, "!=", "")
+    .get();
 
-  console.log(configurationResult)
+  console.log(configurationResult);
 
-  const configuration = configurationResult.docs[0]
+  const configuration = configurationResult.docs[0];
 
   if (configuration.exists == false) {
     console.log("configuration doesn't exist");
-    res.status(401).end('Not authorised');
+    res.status(401).end("Not authorised");
   }
 
   // Now we can forward the request.
   await fetch(configuration[id], {
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    method: "POST", body: JSON.stringify(body.payload)
-  })
+    method: "POST",
+    body: JSON.stringify(body.payload),
+  });
 
-  res.status(200).end('ok')
+  res.status(200).end("ok");
 }
